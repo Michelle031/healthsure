343521bf11d478b25e67e803e7ef39af93d5e111
Michelle031 <mbaangel2803@gmail.com>
2022-09-28 18:07:09 +0100

    alignment

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -5,12 +5,12 @@ import { LockClosedIcon, ClockIcon, BoltIcon, CurrencyDollarIcon, BanknotesIcon,
 function Benefits() {
   return (
     <div id='about' className='w-full px-6 lg:px-14 py-5 my-8'>
-       <div className='flex items-center justify-between py-5 my-7 px-3'>
-        <div className='flex flex-col justify-center space-y-3'>
+       <div className='flex flex-col-reverse lg:flex-row items-center justify-between py-5 my-7 px-3'>
+        <div className='flex flex-col  justify-center space-y-3'>
             <h2 className='text-md lg:text-lg px-4 py-3 rounded-full bg-white w-fit'>Our Benefits</h2>
             <p>We are different. Get benefits from our <br /> insurance benefits you can enjoy with family</p>
         </div>
-        <h1 className='text-3xl ml-2 lg:ml-0 lg:text-6xl'>Benefits Provided By <br/> Our Service</h1>
+        <h1 className='text-3xl mb-3 lg:mb-0 lg:text-6xl'>Benefits Provided By <br/> Our Service</h1>
        </div>
        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center place-content-between '>
         <div className="card group">
